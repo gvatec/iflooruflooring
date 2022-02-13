fc14fecbcc3e3a4ad6b0f5a8af6eda730ef35dbd
gvatec <tecwebcode@gmail.com>
2022-02-13 20:53:09 -0300

    paglogin

---
+++ Loginiflooring/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Iflooruflooring</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="container">
+        <div class="logo">
+            <img src="../img/logotipo.png" alt="logo">
+        </div>
+        <div class="boxlogin">
+            <header>
+             <h1>Login</h1>   
+            </header>
+            <div class="inputs">
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email">
+                    <label for="senha">Password</label>
+                    <input type="password" name="senha" id="senha">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
